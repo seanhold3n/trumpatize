a388bbc088577a6bd41756619b7fa473c241982e
Sean Holden <seanhold3n+github@gmail.com>
2016-11-05 23:29:34 -0400

    Made web binding more friendly for deployment.

--- website/api_server.py
+++ website/api_server.py
@@ -7,6 +7,7 @@ from flask import Flask
 from flask import Response
 from flask import request, redirect, url_for, render_template
 import cv2
+import os
 import trumpatize
 import numpy
 
@@ -47,4 +48,5 @@ def api_about():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
